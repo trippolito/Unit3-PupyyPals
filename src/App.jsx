eed923008174daf4e4a90ec726b3103467d6df4c
Travyynn Ippolito <ippolito3717@gmail.com>
2023-11-13 17:15:21 -0500

    started designing css color scheme + layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { puppyList } from './data.js'
 import './App.css'
 
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
