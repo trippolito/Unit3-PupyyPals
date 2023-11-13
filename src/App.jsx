fe43813ad4ed26525b2deba85081b01cd4834840
Travyynn Ippolito <ippolito3717@gmail.com>
2023-11-13 16:15:29 -0500

    rendered puppies to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
-
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies)
   return (
     <div>
-      <h3>Hello World!</h3>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+      })
+      }
     </div>
   )
 }
