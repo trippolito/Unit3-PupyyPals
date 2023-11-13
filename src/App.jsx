9206b73651219ff8b14bc2e843f205c5ee339344
Travyynn Ippolito <ippolito3717@gmail.com>
2023-11-13 16:58:48 -0500

    added feature that allows cliced names to render all selected arrayy item on screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import { useState } from 'react'
 import { puppyList } from './data.js'
 import './App.css'
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+  // console.log(puppies)
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>
+        return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
       })
       }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )
+      }
+
     </div>
+
   )
 }
 
